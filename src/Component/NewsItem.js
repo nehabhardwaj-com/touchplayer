32b3780be1ea6f497e94bc940a4f36676d73a1e2
nehabhardwaj-com <nehabhardwaj690@gmail.com>
2022-02-24 12:15:21 +0000

    vdo 27

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -6,16 +6,18 @@ export class NewsItem extends Component {
       console.log(source);
       const mystyle = {
           height:150,
+          padding :0,
+          margin:0,
       }
       const mystyle2 = {
         height:80,
     }
     return (
      <>
-     <div className="card mx-3 my-3" id="card">
-            <img src={imageUrl} className="card-img-top" alt="russia" style={mystyle}/>
+     <div className="card  my-3" id="card">
+     <a href={source} target='_blank'><img src={imageUrl?imageUrl:"https://cdn.arstechnica.net/wp-content/uploads/2021/09/iPhone-13-mini-760x380.jpeg"} className="card-img-top" alt="russia" style={mystyle}/></a>
             <div className="card-body">
-                <h5 className="card-title" style={mystyle2}>{title}...</h5>
+                <h5 className="card-title">{title}...</h5>
                 <p className="card-text" >{description}...</p>
                 <a href={source} target='_blank' className="btn btn-sm btn-primary">Read More</a>
             </div>
